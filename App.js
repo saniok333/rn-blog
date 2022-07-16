16e6748ecb48fab06308416c2b7d11492a975ec9
saniok333 <alex.o.csc@gmail.com>
2022-07-16 22:28:14 +0300

    Wrapping the Navigator

--- App.js
+++ App.js
@@ -14,4 +14,8 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
