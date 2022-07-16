b0fdda4f85200bbde9a9f0f1b91f55e80d41a66f
saniok333 <alex.o.csc@gmail.com>
2022-07-16 22:23:37 +0300

    Navigation setup

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
