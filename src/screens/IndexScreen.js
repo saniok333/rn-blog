0ab6a114335f5338dd7fef9d8bd31fae23adf624
saniok333 <alex.o.csc@gmail.com>
2022-07-17 12:46:00 +0300

    Rendering a List of Posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,20 @@
 import { useContext } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
 
   return (
     <View>
       <Text>IndexScreen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(post) => post.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
