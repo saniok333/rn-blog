b493878a7aad1594ee1791f1f8c664b9293df81c
saniok333 <alex.o.csc@gmail.com>
2022-07-18 18:23:26 +0300

    Added initial CreateScreen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+import React from 'react';
+
+const CreateScreen = () => {
+  return (
+    <View>
+      <Text>CreateScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
