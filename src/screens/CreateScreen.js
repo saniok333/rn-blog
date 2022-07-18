775af04df1a2ce9909162a75e0c49bf6387e07a3
saniok333 <alex.o.csc@gmail.com>
2022-07-18 22:33:30 +0300

    Displaying a Form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,17 +1,41 @@
-import { useContext } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useContext, useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 import React from 'react';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
     <View>
-      <Text>CreateScreen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput style={styles.input} onChangeText={setTitle} value={title} />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setContent}
+        value={content}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    margin: 5,
+    marginBottom: 15,
+    padding: 5,
+  },
+});
 
 export default CreateScreen;
