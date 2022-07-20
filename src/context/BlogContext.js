06758cb3c1bb25e2e6f2c2751fca92e387fdb533
saniok333 <alex.o.csc@gmail.com>
2022-07-20 15:44:12 +0300

    Creating Posts with Post Requests

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -40,9 +40,9 @@ const getBlogPosts = (dispatch) => {
   };
 };
 
-const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+const addBlogPost = () => {
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
     if (callback) {
       callback();
     }
